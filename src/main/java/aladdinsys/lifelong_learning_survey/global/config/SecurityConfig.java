d6c5fc3effb6bb4da5a09c8f4f1f09d9ea5ad0fb
na <nadk@aladdinsys.co.kr>
2024-01-19 14:14:24 +0900

    시큐리티 컨피그 까지 끝

--- src/main/java/aladdinsys/lifelong_learning_survey/global/config/SecurityConfig.java
+++ src/main/java/aladdinsys/lifelong_learning_survey/global/config/SecurityConfig.java
@@ -1,4 +1,37 @@
 package aladdinsys.lifelong_learning_survey.global.config;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import aladdinsys.lifelong_learning_survey.global.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+
+@Configurable
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final JwtAuthenticationFilter jwtAuthFilter;
+
+	private final AuthenticationProvider authenticationProvider;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/aladdinsys/**").permitAll())
+			.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+			.authenticationProvider(authenticationProvider)
+			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
 }
