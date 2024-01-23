7a7630667ce51adb9c42c36f1775156851b984f2
na <nadk@aladdinsys.co.kr>
2024-01-23 15:02:09 +0900

    SpotlessApply

--- src/main/java/aladdinsys/lifelong_learning_survey/global/exception/CustomExceptionHandler.java
+++ src/main/java/aladdinsys/lifelong_learning_survey/global/exception/CustomExceptionHandler.java
@@ -2,14 +2,11 @@
 package aladdinsys.lifelong_learning_survey.global.exception;
 
 import aladdinsys.lifelong_learning_survey.global.response.ErrorResponseBody;
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -23,14 +20,18 @@ public class CustomExceptionHandler {
   public ResponseEntity<ErrorResponseBody> handleCustomException(CustomException e) {
 
     LOGGER.error("CustomException", e);
-    var errorResponseBody = ErrorResponseBody.of(e.getErrorCode().getHttpStatus(), e.getErrorCode().getMessage());
+    var errorResponseBody =
+        ErrorResponseBody.of(e.getErrorCode().getHttpStatus(), e.getErrorCode().getMessage());
 
     return new ResponseEntity<>(errorResponseBody, e.getErrorCode().getHttpStatus());
   }
 
   @ExceptionHandler(NoResourceFoundException.class)
-  public ErrorResponseBody handleException(Exception e) {
+  public ResponseEntity<ErrorResponseBody> handleException(Exception e) {
+
     LOGGER.error("NoResourceFoundException", e);
-    return ErrorResponseBody.of(HttpStatus.NOT_FOUND, "해당 경로를 찾지 못했습니다. url 을 확인해주세요");
+    return new ResponseEntity<>(
+        ErrorResponseBody.of(HttpStatus.NOT_FOUND, "해당 경로를 찾지 못했습니다. url 을 확인해주세요"),
+        HttpStatus.NOT_FOUND);
   }
 }
