d836d2c759afdb1bae262ca99f555baa8ce5c14f
na <nadk@aladdinsys.co.kr>
2024-03-06 14:55:09 +0900

    RepositoryRestResource false

--- src/main/java/aladdinsys/aladdin_survey/domains/auth/repository/ApiKeyRepository.java
+++ src/main/java/aladdinsys/aladdin_survey/domains/auth/repository/ApiKeyRepository.java
@@ -3,7 +3,9 @@ package aladdinsys.aladdin_survey.domains.auth.repository;
 
 import aladdinsys.aladdin_survey.domains.auth.entity.ApiKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface ApiKeyRepository extends JpaRepository<ApiKey, Long> {
 
   ApiKey findByKey(String key);
